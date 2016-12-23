e94f3e039c2fdbf3b9123d2d08083a403754ef4c
mattmcd <matt@matt-mcdonnell.com>
2016-12-23 12:44:17 +0000

    Save and reuse compiled model.  Need to manually delete pkl file to recompile.

--- two_compartment.py
+++ two_compartment.py
@@ -1,10 +1,16 @@
 import pystan
 import numpy as np
+import os
+import pickle
+
 
 # Two compartment model from
 # "Stan: A probabilistic programming language for
 #  Bayesian inference and optimization" Gelman, Lee, Guo (2015)
 # http://www.stat.columbia.edu/~gelman/research/published/stan_jebs_2.pdf
+
+model_file = 'two_compartment.stan'
+pkl_file = 'two_compartment.pkl'
 
 a = np.array([0.8, 1.0])
 b = np.array([2, 0.1])
@@ -19,9 +25,16 @@ y_pred = a[0]*np.exp(-b[0]*x) + a[1]*np.exp(-b[1]*x)
 # Include multiplicative noise
 y = y_pred * np.exp(np.random.normal(0, sigma, N))
 
-# Compile and sample model
-fit = pystan.stan(file='two_compartment.stan',
-                  data={'N': N, 'x': x, 'y': y},
+if os.path.isfile(pkl_file):
+    # Reuse previously compiled model
+    sm = pickle.load(open(pkl_file, 'rb'))
+else:
+    # Compile and sample model
+    sm = pystan.StanModel(file='two_compartment.stan')
+    with open(pkl_file, 'wb') as f:
+        pickle.dump(sm, f)
+
+fit = sm.sampling(data={'N': N, 'x': x, 'y': y},
                   iter=1000, chains=4)
 
 # Plot parameter estimates of interest
