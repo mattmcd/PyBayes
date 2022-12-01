012984a3439efda431b59e3031d645f45eaf64f3
mattmcd <matt@matt-mcdonnell.com>
2022-12-01 13:42:34 +0000

    Advent of Code 2022: day 01 - SQLAlchemy version, refactoring, adding repr methods

--- scripts/aoc2022.py
+++ scripts/aoc2022.py
@@ -7,15 +7,19 @@ from sqlalchemy.exc import NoSuchTableError
 
 DATA_DIR = os.path.expanduser('~/Work/Data/AoC2022')
 
-ENGINE = create_engine(f'sqlite:///{os.path.expanduser("~/Work/Data/AoC2022/aoc2022.db")}')
-
-METADATA = MetaData()
-
 
 def read_input(day):
     with open(os.path.join(DATA_DIR, f'aoc2022_day{day:02}.txt'), 'r') as f:
         return f.read()
 
+
+def day_01():
+    # Short version
+    vals = sorted(
+        [sum([int(x) for x in line.split('\n')])
+         for line in read_input(1).strip().split('\n\n')]
+    )[::-1]
+    return vals[0], sum(vals[:3])
 
 class Day01:
     def __init__(self):
@@ -44,6 +48,8 @@ class Day01:
         # Not part of the puzzle but a nice way of inverting the map
         return max(self.sum_carried, key=self.sum_carried.get)
 
+    def __repr__(self):
+        return f'Part 1: {self.part1()}\nPart 2: {self.part2()}'
 
 class Day01Pandas(Day01):
     def __init__(self):
@@ -57,7 +63,7 @@ class Day01Pandas(Day01):
         df = pd.DataFrame(
             pd.concat([pd.Series(inventory[i], name=i) for i in range(n_elves)], keys=range(n_elves)),
         ).reset_index()
-        df.columns = columns=['elf_id', 'item', 'value']
+        df.columns = ['elf_id', 'item', 'value']
         return df
 
     def part1(self):
@@ -70,15 +76,17 @@ class Day01Pandas(Day01):
 class Day01Sqla(Day01Pandas):
     def __init__(self):
         super(Day01Sqla, self).__init__()
+        self.engine = create_engine(f'sqlite:///{os.path.join(DATA_DIR, "aoc2022.db")}')
+        self.metadata = MetaData()
         self.inventory = self.parse_sqla()
 
     def parse_sqla(self):
         try:
-            tb_inventory = Table('Day01', METADATA, autoload=True, autoload_with=ENGINE)
+            tb_inventory = Table('Day01', self.metadata, autoload=True, autoload_with=self.engine)
         except NoSuchTableError:
             inventory = self.inventory
-            inventory.to_sql('Day01', ENGINE, if_exists='fail', index=False)
-            tb_inventory = Table('Day01', METADATA, autoload=True, autoload_with=ENGINE)
+            inventory.to_sql('Day01', self.engine, if_exists='fail', index=False)
+            tb_inventory = Table('Day01', self.metadata, autoload=True, autoload_with=self.engine)
         return tb_inventory
 
     def sum_by_elf(self):
@@ -88,11 +96,11 @@ class Day01Sqla(Day01Pandas):
 
     def part1(self):
         query = self.sum_by_elf().limit(1)
-        print(str(query))
-        return pd.read_sql(query, ENGINE)['sum_value'][0]
+        # print(str(query))
+        return pd.read_sql(query, self.engine)['sum_value'][0]
 
     def part2(self):
         query_1 = self.sum_by_elf().limit(3).cte('query_1')
         query = select(func.sum(query_1.c.sum_value).label('sum_value'))
-        print(str(query))
-        return pd.read_sql(query, ENGINE)['sum_value'][0]
+        # print(str(query))
+        return pd.read_sql(query, self.engine)['sum_value'][0]
