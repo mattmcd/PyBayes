382d0f33f7f50bd5ae9a1eca94406e78c31b2a3d
mattmcd <matt@matt-mcdonnell.com>
2025-08-30 10:42:58 +0100

    Refactor into class

--- scripts/journal_20250829_aws.py
+++ scripts/journal_20250829_aws.py
@@ -5,51 +5,66 @@ from botocore.exceptions import ClientError
 import os
 
 # %%
-ip = requests.get('https://checkip.amazonaws.com').text.strip()
-# %%
-ec2 = boto3.resource('ec2')
+def auth_current_ip(security_group, ip):
+    try:
+        security_group.authorize_ingress(
+            DryRun=False,
+            IpPermissions=[
+                {
+                    'FromPort': 22,
+                    'ToPort': 22,
+                    'IpProtocol': 'tcp',
+                    'IpRanges': [
+                        {
+                            'CidrIp': f'{ip}/32',
+                        }
+                    ]
+                }
+            ]
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'InvalidPermission.Duplicate':
+            print('Rule already exists')
+        else:
+            raise e
+
 
 # %%
-security_group = ec2.SecurityGroup(os.environ['AWS_SECURITY_GROUP_ID'])
+def revoke_old_ips(security_group, new_ip):
+    ip_permissions = security_group.ip_permissions
+    old_ips = [
+        r['CidrIp'] for r in ip_permissions[0]['IpRanges'] if new_ip not in r['CidrIp']
+    ]
+    # Revoke access for all other IP addresses
+    for old_ip in old_ips:
+        security_group.revoke_ingress(
+            CidrIp=old_ip,
+            FromPort=22,
+            ToPort=22,
+            IpProtocol='tcp',
+        )
+    print(f'Revoked access for {len(old_ips)} old IP addresses')
+
 
 # %%
-try:
-    security_group.authorize_ingress(
-        DryRun=False,
-        IpPermissions=[
-            {
-                'FromPort': 22,
-                'ToPort': 22,
-                'IpProtocol': 'tcp',
-                'IpRanges': [
-                    {
-                        'CidrIp': f'{ip}/32',
-                    }
-                ]
-            }
-        ]
-    )
-except ClientError as e:
-    if e.response['Error']['Code'] == 'InvalidPermission.Duplicate':
-        print('Rule already exists')
-    else:
-        raise e
+class SecurityGroupIpUpdater:
+    def __init__(self, security_group=None, ip=None):
+        self.security_group = security_group
+        self.ip = ip
+
+    def update(self):
+        auth_current_ip(self.security_group, self.ip)
+        revoke_old_ips(self.security_group, self.ip)
+
+    @classmethod
+    def from_env(cls):
+        return cls(
+            security_group=boto3.resource('ec2').SecurityGroup(os.environ['AWS_SECURITY_GROUP_ID']),
+            ip=requests.get('https://checkip.amazonaws.com').text.strip()
+        )
+
 
 # %%
-ip_permissions = security_group.ip_permissions
-
-# %%
-old_ips = [r['CidrIp'] for r in ip_permissions[0]['IpRanges'] if ip not in r['CidrIp']]
-
-# %%
-# Revoke access for all other IP addresses
-for old_ip in old_ips:
-    security_group.revoke_ingress(
-        CidrIp=old_ip,
-        FromPort=22,
-        ToPort=22,
-        IpProtocol='tcp',
-    )
-
-print(f'Revoked access for {len(old_ips)} old IP addresses')
-
+if __name__ == '__main__':
+    updater = SecurityGroupIpUpdater.from_env()
+    updater.update()
