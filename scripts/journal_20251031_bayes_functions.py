be10869ba5c0cc62d1dfca09825243f353157b6f
mattmcd <matt@matt-mcdonnell.com>
2025-11-04 07:47:24 +0000

    Extract data aggregation function

--- scripts/journal_20251031_bayes_functions.py
+++ scripts/journal_20251031_bayes_functions.py
@@ -1,6 +1,39 @@
 from collections import defaultdict
 import itertools
 import pandas as pd
+from typing import Callable
+
+
+def aggregate_data(df: pd.DataFrame, features: list[str], target: str, label_fun: Callable) -> pd.DataFrame:
+    """
+    Aggregates the provided dataframe by grouping based on a subset of
+    features, and generates trial and success statistics for each group.
+    The method applies a label function on the data to determine
+    success conditions and subsequently aggregates data based on specified
+    grouping criteria.
+
+    :param df: The input DataFrame containing the data to be aggregated.
+        It must include the columns specified in the features parameter.
+    :param features: A list of column names in the DataFrame that will be
+        used for grouping the data. The target column must also be included
+        in this list.
+    :param target: The primary column in `features` that identifies the
+        specific feature column for evaluation of success conditions.
+    :param label_fun: A callable function that determines the success condition
+        for each row in the provided DataFrame. Returns a boolean-like result
+        indicating success or failure for a given row.
+    :return: A DataFrame with aggregated data, containing columns for the
+        number of trials (`trials`) and the number of successes (`successes`)
+        based on the provided grouping criteria and label function.
+    """
+    return df.loc[:, features].assign(
+        label=label_fun
+    ).groupby(
+        [c for c in features if c != target]
+    ).agg(
+        trials=pd.NamedAgg(column='label', aggfunc='count'),
+        successes=pd.NamedAgg(column='label', aggfunc='sum')
+    )
 
 
 def extract_sub_population(df, sub_pop):
@@ -24,7 +57,7 @@ def extract_sub_population(df, sub_pop):
     except KeyError:
         # Not multi-indexed so not suitable for further sub-setting but
         # prevents bayes_rule from failing
-        return pd.DataFrame({'trials': [0], 'successes': [0]},)
+        return pd.DataFrame({'trials': [0], 'successes': [0]}, )
 
 
 def bayes_rule(df, df_s):
@@ -46,13 +79,13 @@ def bayes_rule(df, df_s):
 
 def long_summary(pop_slice, target, p_target, p_sub_pop, p_target_given_sub_pop, p_sub_pop_given_target):
     """Summary of sub-population probabilities including level values in description"""
-    slice_str = ', '.join({ k: f'{k}={" or ".join(v)}' for k, v in pop_slice.items()}.values())
+    slice_str = ', '.join({k: f'{k}={" or ".join(v)}' for k, v in pop_slice.items()}.values())
     return f'{pop_slice}\n' \
            f'P({target}) = {p_target:.3f}\n' \
            f'P({slice_str}) = {p_sub_pop:.3f}\n' \
            f'P({target} | {slice_str}) = {p_target_given_sub_pop:.3f}\n' \
            f'P({slice_str} | {target}) = {p_sub_pop_given_target:.3f}\n' \
-           f'Odds ratio = {p_sub_pop_given_target/(1 - p_sub_pop_given_target):.3f}:1'
+           f'Odds ratio = {p_sub_pop_given_target / (1 - p_sub_pop_given_target):.3f}:1'
 
 
 def brief_summary(pop_slice, target, p_target, p_sub_pop, p_target_given_sub_pop, p_sub_pop_given_target):
@@ -62,7 +95,7 @@ def brief_summary(pop_slice, target, p_target, p_sub_pop, p_target_given_sub_pop
            f'P({", ".join(pop_slice.keys())}) = {p_sub_pop:.3f}\n' \
            f'P({target} | {", ".join(pop_slice.keys())}) = {p_target_given_sub_pop:.3f}\n' \
            f'P({", ".join(pop_slice.keys())} | {target}) = {p_sub_pop_given_target:.3f}\n' \
-           f'Odds ratio = {p_sub_pop_given_target/(1 - p_sub_pop_given_target):.3f}:1'
+           f'Odds ratio = {p_sub_pop_given_target / (1 - p_sub_pop_given_target):.3f}:1'
 
 
 class BayesRule:
@@ -99,4 +132,4 @@ def marginalize(df, groups):
     df_marginal_probs = pd.DataFrame(
         [sp | {'prob': BayesRule(df, sp).p_sub_pop_given_target} for sp in sub_pops]
     )
-    return df_marginal_probs   # .fillna(0)  # leaving in NaN makes excluded groups more obvious
+    return df_marginal_probs  # .fillna(0)  # leaving in NaN makes excluded groups more obvious
